abb3e2ec46d1bc47a01223d7311947e49c1d0e26
Matías Sánchez <matiassanchez_@outlook.com>
2025-03-19 22:28:01 -0300

    fix: Cambios en la sección principal
    Se cambiaron los botones iniciales de contacto a linkedin

--- src/sections/sectionmain.jsx
+++ src/sections/sectionmain.jsx
@@ -9,11 +9,20 @@ export default function SectionMain() {
                         <h1 className="text-5xl font-bold">Matías Sánchez</h1>
                         <h2 className="text-3xl font-bold text-right">Portfolio</h2>
                         <p className="py-6">
-                            
+
                         </p>
-                        <a href="#proyectos" className="btn btn-accent w-32 m-2">Ver CV</a>
-                        <a href="#proyectos" className="btn btn-secondary w-32 m-2">Contacto</a>
-                        <a href="#proyectos" className="btn btn-primary w-32 m-2">Proyectos</a>
+                        <a href="#proyectos" className="btn btn-primary w-32 m-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-hammer"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M11.414 10l-7.383 7.418a2.091 2.091 0 0 0 0 2.967a2.11 2.11 0 0 0 2.976 0l7.407 -7.385" /><path d="M18.121 15.293l2.586 -2.586a1 1 0 0 0 0 -1.414l-7.586 -7.586a1 1 0 0 0 -1.414 0l-2.586 2.586a1 1 0 0 0 0 1.414l7.586 7.586a1 1 0 0 0 1.414 0z" /></svg>
+                            Proyectos
+                        </a>
+                        <a href="#proyectos" className="btn btn-accent w-32 m-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-certificate"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M5 8v-3a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-5" /><path d="M6 14m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M4.5 17l-1.5 5l3 -1.5l3 1.5l-1.5 -5" /></svg>
+                            Ver CV
+                        </a>
+                        <a href="https://www.linkedin.com/in/matiassanchezbaez/" target="_blank" className="btn btn-secondary w-32 m-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-brand-linkedin"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M17 2a5 5 0 0 1 5 5v10a5 5 0 0 1 -5 5h-10a5 5 0 0 1 -5 -5v-10a5 5 0 0 1 5 -5zm-9 8a1 1 0 0 0 -1 1v5a1 1 0 0 0 2 0v-5a1 1 0 0 0 -1 -1m6 0a3 3 0 0 0 -1.168 .236l-.125 .057a1 1 0 0 0 -1.707 .707v5a1 1 0 0 0 2 0v-3a1 1 0 0 1 2 0v3a1 1 0 0 0 2 0v-3a3 3 0 0 0 -3 -3m-6 -3a1 1 0 0 0 -.993 .883l-.007 .127a1 1 0 0 0 1.993 .117l.007 -.127a1 1 0 0 0 -1 -1" /></svg>
+                            Linkedin
+                        </a>
                     </div>
                 </div>
             </div>
